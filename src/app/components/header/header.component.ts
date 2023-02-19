54cea85d323c1e712667033a591695d3bae2436a
josafamarengo <josafabmarengo@gmail.com>
2023-02-19 11:13:59 -0400

    feat: structure and layout

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-bar',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class MenuBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
